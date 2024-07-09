03b98060e0f48ab2c9b1f1b9b4e22219e59b415b
Hammad Majid <hammadmajid@proton.me>
2024-07-09 13:16:36 +0500

    add: talwindcss/typography plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        'IBMPlexMono': ['IBMPlexMono', ...defaultTheme.fontFamily.mono],
-      }
+        IBMPlexMono: ['IBMPlexMono', ...defaultTheme.fontFamily.mono],
+      },
     },
     colors: {
       'light-body': '#fbfefb',
@@ -15,7 +15,7 @@ module.exports = {
       'light-text-tertiary': '#6f6f6f',
       'light-accent': '#1678b3',
       'light-accent-hover': '#0f5f8b',
-    }
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
