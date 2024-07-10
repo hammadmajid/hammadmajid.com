da522e9289748e2053d3dd2fd1914b30189ba7aa
Hammad Majid <hammadmajid@proton.me>
2024-07-11 00:31:42 +0500

    add: new color for border in light mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,7 @@ module.exports = {
       'light-text-tertiary': '#6f6f6f',
       'light-accent': '#1678b3',
       'light-accent-hover': '#0f5f8b',
+      'light-border': '#d4d4d4',
     },
   },
   plugins: [require('@tailwindcss/typography')],
