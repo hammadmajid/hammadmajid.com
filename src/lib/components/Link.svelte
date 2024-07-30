bcad2e7795f448637f886ac9a36417c7d7af780c
Hammad Majid <hammadmajid@proton.me>
2024-07-30 23:27:06 +0500

    Allow consumer to specify weight of the font in `Link` component

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	interface Props {
 		href: string;
+		fontWeight?: string;
 		children: import('svelte').Snippet;
 	}
 
-	let { href, children }: Props = $props();
+	let { href, fontWeight: weightOverride = 'font-medium', children }: Props = $props();
 </script>
 
 <a
-	class="underline underline-offset-4 decoration-dashed font-medium hover:text-blue-500 transition-all duration-350"
+	class={'underline font-inherit underline-offset-4 decoration-dashed hover:text-blue-500 transition-all duration-350' +
+		' ' +
+		weightOverride}
 	{href}>{@render children()}</a
 >
