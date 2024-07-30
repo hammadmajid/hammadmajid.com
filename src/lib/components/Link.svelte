ffd6e2b86dc74fa6975da2cee4cd6de92ca8a348
Hammad Majid <hammadmajid@proton.me>
2024-07-30 22:09:06 +0500

    Create genric Link component to render <a> tag

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface Props {
+		href: string;
+		children: import('svelte').Snippet;
+	}
+
+	let { href, children }: Props = $props();
+</script>
+
+<a
+	class="underline underline-offset-4 decoration-dashed font-medium hover:text-blue-500 transition-all duration-350"
+	{href}>{@render children()}</a
+>
